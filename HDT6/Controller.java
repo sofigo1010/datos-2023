159d88f8ae329d0c02908d9846b93a194b75da01
sofia g <110646163+sofigo1010@users.noreply.github.com>
2023-03-05 22:13:22 -0600

    2

--- HDT6/Controller.java
+++ HDT6/Controller.java
@@ -8,5 +8,74 @@
 import java.util.Map;
 
 public class Controller {
+    View vista = new View();
+    int choice;
+    int choice2;
+    Read reads = new Read();
+    Map<Integer, Producto> inventario;
+    Map<Integer, Producto> carritodecompras;
+    boolean start = true;
+    Productmanager manager = new Productmanager();
+    
+    public void supermercado(){
+        choice = vista.menumaps();
+        inventario = MapFactory.crearmapa(choice);
+        carritodecompras = MapFactory.crearmapa(choice);
+        reads.getinventory(inventario, "ListadoProducto (2).txt");
+        while (start == true){
+            choice2 = vista.menusupermercado();
+            if (choice2 == 1){
+                //agregar al carrito
+                String nombre = vista.nombreproductnuevo();
+                String categoria =vista.categoriaproductonuevo();
+                int cantidad = vista.cantidadproductonuevo();
+                boolean encontrado = false;
+                for (Map.Entry<Integer, Producto> entry : inventario.entrySet()) {
+                    Producto producto = entry.getValue();
+                    if (producto.getNombre().equals(nombre) && producto.getCategoria().equals(categoria)) {
+                        manager.agregaralcarrito(carritodecompras, nombre, categoria, cantidad);
+                        encontrado = true;
+                    }
+                }
+                if (!encontrado) {
+                    vista.noencuentro();;
+                }
+            }
 
+            else if (choice2 == 2){
+                //Ver la categoría de un producto
+                String producto = vista.nombreproductnuevo();
+                
+                
+            }
+            else if (choice2 == 3){
+                //ver los productos del carrito
+                
+            }
+            else if (choice2 == 4){
+                //ver los productos del carrito ordenados
+                
+                
+            }
+            else if (choice2 == 5){
+                //ver todos los productos del inventario
+                
+                
+            }
+            else if (choice2 == 6){
+                //ver todos los productos del inventario ordenados
+                
+            }
+            else if (choice2 == 7){
+                
+    
+            }
+            else{
+                
+                
+            }
+
+        }
+
+    }
 }
--- HDT6/View.java
+++ HDT6/View.java
@@ -8,6 +8,67 @@
 import java.util.Scanner;
 
 public class View {
+    int respuesta; 
+    Scanner sc = new Scanner(System.in);
+    String respuesta2;
+    
+    /** menú inicial para escoger el tipo de mapa a utilizar 
+     * 
+     * @return int
+     */
+    public int menumaps (){
+        System.out.println("Bienvenido al SuperMercado Enchilada Nicolita!\n\n\nCon qué tipo de Mapa desea visualizar el inventario?\n1.HashMap\n2. TreeMap\n3. LinkedHashMap\nIngrese el número de índice: ");
+        respuesta = sc.nextInt();
+        return respuesta;
+    }
+    
+    /** Opciones del menu 
+     * @return int
+     */
+    public int menusupermercado(){
+        System.out.println("\t\t\t\t\t\tSUPERMERCADO ENCHILADA NICOLITA\n\nQué desea hacer?\n1. Agregar un producto a su carrito de compras\n2. Ver la categoría de un producto\n3. Ver los datos de los productos en su carrito\n4. Ver los datos de los productos en su carrito ordenados por categoría\n5. Ver todos los productos y categorías del inventario\n6. Ver todos los productos y categorías del inventario ordenados por tipo\n7. Salir del programa\n\nIngrese el número de índice");
+        respuesta = sc.nextInt();
+        return respuesta;
+    }
+
+    
+    /** ingresar la categoria de un producto que quiere agregar a su carrito de compras
+     * @return String
+     */
+    public String categoriaproductonuevo(){
+        System.out.println("Ingresa la categoría del producto");
+        respuesta2 = sc.next();
+        return respuesta2;
+    }
+
+    
+    /** ingresar el nombre de un producto que quiere agregar a su carrito de compras
+     * @return String
+     */
+    public String nombreproductnuevo(){
+        System.out.println("Ingresa el nombre del producto");
+        respuesta2 = sc.next();
+        respuesta2 += sc.nextLine();
+        return respuesta2;
+    }
+
+    
+    /** ingresar la cantidad de un producto que quiere agregar a su carrito de compras
+     * @return int
+     */
+    public int cantidadproductonuevo(){
+        System.out.println("Ingresa la cantidad del producto que deseas agregar a tu carrito");
+        int respuesta = sc.nextInt();
+        return respuesta;
+    }
+
+    /** 
+     * mensaje a imprimir en caso de que no se encuentre el producto que quiere agregar
+     */
+    public void noencuentro(){
+        System.out.println("No se encontró un producto con ese nombre y categoría en el inventario");
+    }
+
 
     
 
